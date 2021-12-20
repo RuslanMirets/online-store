f4ec78958269ab4cf39408fd9a1f487292f1f9ab
Ruslan Mirets <ruslan.mirets@gmail.com>
2021-12-20 10:43:36 +0300

    [BACKEND] - Модели данных и связи между ними

--- server/index.js
+++ server/index.js
@@ -1,18 +1,22 @@
 require('dotenv').config();
 const express = require('express');
 const sequelize = require('./db');
+const models = require('./models/models');
 const PORT = process.env.PORT || 5000;
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-const satrt = async () => {
+const start = async () => {
   try {
-    await sequelize.authenticate()
-    await sequelize.sync()
+    await sequelize.authenticate();
+    await sequelize.sync();
     app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
   } catch (error) {
     console.log(error);
   }
 };
 
-satrt();
+start();
